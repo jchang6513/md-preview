2ad5d74e11e9dd24cbf810ebe81b5e557cdbaa76
Yi-Wei Chang <jchang6513@gmail.com>
2023-06-22 21:48:47 +0800

    feat: apply style

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,5 +1,6 @@
 'use strict';
 const showdown = require('showdown');
+const showdownHighlight = require("showdown-highlight");
 
 // chrome.runtime.sendMessage(
 //   {
@@ -13,13 +14,53 @@ const showdown = require('showdown');
 //   }
 // );
 
-const converter = new showdown.Converter();
+const converter = new showdown.Converter(
+  {
+    extensions: [showdownHighlight({
+        pre: true,
+    })]
+  }
+);
+
+let mdHTML = ''
+let mdText = ''
+
+const isIncludePRE = () => document.body && (document.body.childNodes[0] && document.body.childNodes[0].tagName == "PRE")
+
+const mdToHtml = () => {
+  const textElement = document.body.childNodes[0]
+  if (!mdText) {
+    mdText = textElement.innerText
+  }
+  if (!mdHTML) {
+    mdHTML = converter.makeHtml(mdText);
+  }
+  textElement.innerHTML = mdHTML;
+  textElement.style = 'white-space: unset; word-wrap: unset; max-width: 1012px; margin: auto';
+}
+
+const applyGlobalStyle = () => {
+  const style = document.createElement('style');
+  style.innerHTML = `
+    body { font-size: 16px }
+    a { text-decoration: none; }
+    code { background: #f8f8f8; }
+  `;
+  document.body.appendChild(style);
+}
+
+const applyHighlightStyle = () => {
+  const githubCss = document.createElement('link');
+    githubCss.rel = 'stylesheet';
+    githubCss.href = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/github-dark-dimmed.min.css';
+    document.head.appendChild(githubCss);
+}
 
 async function init() {
-	if (document.body && (document.body.childNodes[0] && document.body.childNodes[0].tagName == "PRE" || document.body.children.length == 0)) {
-		const textElement = document.body.children.length ? document.body.childNodes[0] : document.body;
-    const htmlContent = converter.makeHtml(textElement.innerText);
-    textElement.innerHTML = htmlContent;
+	if (isIncludePRE()) {
+    applyHighlightStyle()
+    applyGlobalStyle()
+    mdToHtml()
 	}
 }
 
