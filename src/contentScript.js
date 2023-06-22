255ce38f89ec2637c56b4ce24394003ca9bb14dd
Yi-Wei Chang <jchang6513@gmail.com>
2023-06-22 15:10:59 +0800

    feat: convert md to html

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,43 +1,26 @@
 'use strict';
+const showdown = require('showdown');
 
-// Content script file will run in the context of web page.
-// With content script you can manipulate the web pages using
-// Document Object Model (DOM).
-// You can also pass information to the parent extension.
+// chrome.runtime.sendMessage(
+//   {
+//     type: 'messagetype',
+//     payload: {
+//       message: 'message',
+//     },
+//   },
+//   (response) => {
+//     console.log(response.message);
+//   }
+// );
 
-// We execute this script by making an entry in manifest.json file
-// under `content_scripts` property
+const converter = new showdown.Converter();
 
-// For more information on Content Scripts,
-// See https://developer.chrome.com/extensions/content_scripts
+async function init() {
+	if (document.body && (document.body.childNodes[0] && document.body.childNodes[0].tagName == "PRE" || document.body.children.length == 0)) {
+		const textElement = document.body.children.length ? document.body.childNodes[0] : document.body;
+    const htmlContent = converter.makeHtml(textElement.innerText);
+    textElement.innerHTML = htmlContent;
+	}
+}
 
-// Log `title` of current active web page
-const pageTitle = document.head.getElementsByTagName('title')[0].innerHTML;
-console.log(
-  `Page title is: '${pageTitle}' - evaluated by Chrome extension's 'contentScript.js' file`
-);
-
-// Communicate with background file by sending a message
-chrome.runtime.sendMessage(
-  {
-    type: 'GREETINGS',
-    payload: {
-      message: 'Hello, my name is Con. I am from ContentScript.',
-    },
-  },
-  (response) => {
-    console.log(response.message);
-  }
-);
-
-// Listen for message
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'COUNT') {
-    console.log(`Current count is ${request.payload.count}`);
-  }
-
-  // Send an empty response
-  // See https://github.com/mozilla/webextension-polyfill/issues/130#issuecomment-531531890
-  sendResponse({});
-  return true;
-});
+init();
